fe70435cd2ade1a5c6dc65b4c0887d1a9510cb24
Bryan <btruong10@my.bcit.ca>
2021-03-09 14:59:41 -0800

    made some more changes to the app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+let num1 = 4;
+let num2 = 6;
+const str = "Hello";
+let foo = true;
+
+if(num1 === num2){
+    console.log("test 1: They match");
+}else{
+    console.log("test 1: No luck");  
+}
+
+num1 = 6;
+if(num1 === num2){
+    console.log("test 2: They match");
+}else{
+    console.log("test 2: No luck");  
+}
+
+num2 = "4"
+
+if(num1 === num2){
+    console.log("test 3 (type): They match");
+}else{
+    console.log("test 3 (type): No luck");  
+}
+
+for (letter in str){
+    console.log(str[i]);
+}
